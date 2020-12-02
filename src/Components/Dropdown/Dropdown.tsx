ab83c937487620a172b6533805514e5afc13b9de
estifter <52901093+estifter@users.noreply.github.com>
2020-12-02 17:50:44 -0600

    Added interface for DropdownOption

--- src/Components/Dropdown/Dropdown.tsx
+++ src/Components/Dropdown/Dropdown.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import "./Dropdown.css";
+
+interface DropdownOption {
+    text: string;
+    value: string;
+}
 
 interface DropdownProps {
-    show: boolean;
-    options: { text: string; value: string }[];
+    options: DropdownOption[];
     onSelection(selection: string): void;
 }
 
@@ -10,10 +14,11 @@ export default function Dropdown(props: DropdownProps) {
     // return a function to handle event but allow value to be passed in first
     const handleClick = (value: string) => {
         return (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-            props.onSelection(value)
+            props.onSelection(value);
         };
     };
 
+    // the rendered choices
     const choices = props.options.map((option, index) => {
         return (
             <div key={index} className="dropdown-choice" onClick={handleClick(option.value)}>
