76e4003de13711f04cfdbe68fcbf568116bfb612
estifter <52901093+estifter@users.noreply.github.com>
2020-12-04 19:17:19 -0600

    Added a dropdown component to autocomplete formulas

--- src/Components/DisplayCell/DisplayCell.tsx
+++ src/Components/DisplayCell/DisplayCell.tsx
@@ -2,10 +2,11 @@ import React, { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ViewModes } from "../../constants";
 import { changeElementFormula } from "../../redux/actions";
-import { StateObject } from "../../types";
-import { parser } from "../App/App";
+import { parser, SUPPORTED_FORMULAS } from "../App/App";
 import { Cell } from "../Cell/Cell";
 import { stringIsNumber } from "../../utils/util";
+import Dropdown from "../Dropdown/Dropdown";
+import { StateObject } from "../../redux/store";
 
 interface DisplayCellProps {
     accessID: string;
@@ -26,12 +27,50 @@ export default function DisplayCell(props: DisplayCellProps) {
         return { ...state.elements.byID };
     });
 
+    const uppercaseFormula = element.formula.toUpperCase().split("(").slice(-1)[0]; // create const for performance
+    const dropdownOptions =
+        uppercaseFormula === ""
+            ? []
+            : SUPPORTED_FORMULAS.filter(
+                  (formula: string) => formula.includes(uppercaseFormula) // autocomplete on type
+              )
+                  .sort((a: string, b: string) => {
+                      return a.indexOf(uppercaseFormula) - b.indexOf(uppercaseFormula);
+                  })
+                  .map((formula: string) => {
+                      return { text: formula, value: formula };
+                  });
+
     const handleValueChange = useCallback(
         (event: React.ChangeEvent<HTMLInputElement>) => {
             dispatch(changeElementFormula(element.accessID, event.target.value));
         },
         [dispatch, element.accessID]
     );
+
+    const handleDropdownClick = useCallback(
+        (selection: string) => {
+            dispatch(changeElementFormula(element.accessID, element.formula + selection + "("));
+        },
+        [dispatch, element.accessID, element.formula]
+    );
+
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLElement>) => {
+        if ((event.key === "Enter" || event.key === "Tab") && dropdownOptions.length > 0) {
+            const appendToFront =
+                !element.formula.includes('(')
+                    ? ""
+                    : element.formula.split("(").slice(0, -1).join("(") + // replace everything after last open parentheses
+                      "("; // add parenthesis back after removal during split
+
+            dispatch(
+                changeElementFormula(
+                    element.accessID,
+                    appendToFront + dropdownOptions[0].text + "("
+                )
+            );
+        }
+    };
 
     const getCellValue = () => {
         if (viewMode === ViewModes.formulas) {
@@ -62,13 +101,17 @@ export default function DisplayCell(props: DisplayCellProps) {
     };
 
     return (
-        <div>
+        <div onKeyPress={handleKeyPress}>
             <Cell
                 id={element.userAssignedID}
                 value={getCellValue()}
                 handleValueChange={handleValueChange}
                 readonly={viewMode === ViewModes.values}
+                autocomplete={false}
             />
+            {viewMode === ViewModes.formulas ? (
+                <Dropdown onSelection={handleDropdownClick} options={dropdownOptions} />
+            ) : undefined}
         </div>
     );
 }
