ce7dd65305d1a8d3a67625060503190585e20b81
estifter <52901093+estifter@users.noreply.github.com>
2020-12-08 12:36:22 -0600

    Updated to use new state slices

--- src/Components/InputCell/InputCell.tsx
+++ src/Components/InputCell/InputCell.tsx
@@ -1,32 +1,32 @@
 import React, { useCallback } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { ViewModes } from "../../constants";
-import { changeElementValue } from "../../redux/actions";
-import { StateObject } from "../../redux/store";
+import dataSlice from "../../redux/dataSlice";
+import { RootState } from "../../redux/store";
 import { Cell } from "../Cell/Cell";
 
 interface InputCellProps {
-    accessID: string;
+    id: string;
 }
 
 /**
  * Cell that accepts user-input values.
- * @param accessID: the ID the cell was created with / used to access element data in state.elements.byID
+ * @param id: the ID the cell was created with / used to access element data in state.data.elements
  */
 export default function InputCell(props: InputCellProps) {
     const dispatch = useDispatch();
-    const element = useSelector((state: StateObject) => {
-        return {...state.elements.byID[props.accessID]}; // spread here to assure a new reference so that shallowEqual doesn't short circuit to true
+    const element = useSelector((state: RootState) => {
+        return { ...state.data.elements[props.id] }; // spread here to assure a new reference so that shallowEqual doesn't short circuit to true
     }, shallowEqual);
-    const viewMode = useSelector((state: StateObject) => {
+    const viewMode = useSelector((state: RootState) => {
         return state.application.viewMode;
     });
 
     const handleValueChange = useCallback(
         (event: React.ChangeEvent<HTMLInputElement>) => {
-            dispatch(changeElementValue(props.accessID, event.target.value));
+            dispatch(dataSlice.actions.valueChanged({ id: props.id, value: event.target.value }));
         },
-        [dispatch, props.accessID]
+        [dispatch, props.id]
     );
 
     const getCellValue = () => {
@@ -36,7 +36,7 @@ export default function InputCell(props: InputCellProps) {
 
     return (
         <Cell
-            id={element.userAssignedID}
+            id={props.id}
             value={getCellValue()}
             handleValueChange={handleValueChange}
             readonly={viewMode === ViewModes.formulas}
