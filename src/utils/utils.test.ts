e02c313fabed6bd2bb6416a08de91b271b44204f
estifter <52901093+estifter@users.noreply.github.com>
2020-12-08 12:38:20 -0600

    Added tests for positiveOrZero

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,4 +1,4 @@
-import { numberIsNumber, stringIsNumber } from './util';
+import { numberIsNumber, positiveOrZero, stringIsNumber } from './util';
 
 test('Test number checking functions', () => {
     expect(numberIsNumber(4)).toBe(true);
@@ -9,3 +9,10 @@ test('Test number checking functions', () => {
     expect(stringIsNumber('1.2')).toBe(true);
     expect(stringIsNumber('123f')).toBe(false);
 });
+
+test('Test positiveOrZero', () => {
+    expect(positiveOrZero(2134)).toBe(2134);
+    expect(positiveOrZero(-123)).toBe(0);
+    expect(positiveOrZero(0)).toBe(0);
+    expect(positiveOrZero(NaN)).toBe(NaN);
+})
