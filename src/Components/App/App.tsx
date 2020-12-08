eb8364768dc47c423ea424f7d0effbbc00455c67
estifter <52901093+estifter@users.noreply.github.com>
2020-12-08 12:32:13 -0600

    Update component to use new state slices logic and to handle new moveMode toggling

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,52 +1,63 @@
 import React from "react";
 import "./App.css";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import { addElement, changeViewMode } from "../../redux/actions";
-import { StateObject } from "../../redux/store";
 import { ElementTypes, ViewModes } from "../../constants";
 import InputCell from "../InputCell/InputCell";
 import DisplayCell from "../DisplayCell/DisplayCell";
+import { RootState } from "../../redux/store";
+import dataSlice from "../../redux/dataSlice";
+import applicationSlice from "../../redux/applicationSlice";
 
 // initialize parser object
 var FormulaParser = require("hot-formula-parser").Parser;
 export const SUPPORTED_FORMULAS = require("hot-formula-parser").SUPPORTED_FORMULAS;
 export var parser = new FormulaParser();
 
-interface FormProps {}
-
-function Form(props: FormProps) {
+function Form(props: {}) {
     const dispatch = useDispatch();
-    const elementsByID = useSelector((state: StateObject) => {
-        return state.elements.byID;
+    const elementsByID = useSelector((state: RootState) => {
+        return state.data.elements;
     }, shallowEqual);
-    const allIDs = useSelector((state: StateObject) => {
-        return state.elements.allIDs;
+    const allIDs = useSelector((state: RootState) => {
+        return state.data.ids;
     }, shallowEqual);
-    const viewMode = useSelector((state: StateObject) => {
+    const viewMode = useSelector((state: RootState) => {
         return state.application.viewMode;
     });
 
     // these are temporary for testing
     const handleAddInputCellClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        dispatch(addElement(ElementTypes.inputCell));
+        dispatch(
+            dataSlice.actions.elementAdded({
+                id: "inputCell" + allIDs.length,
+                elementType: ElementTypes.inputCell,
+            })
+        );
     };
     const handleAddDisplayCellClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        dispatch(addElement(ElementTypes.displayCell));
+        dispatch(
+            dataSlice.actions.elementAdded({
+                id: "displayCell" + allIDs.length,
+                elementType: ElementTypes.displayCell,
+            })
+        );
     };
-    const handleToggleFormulaViewClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const handleToggleViewModeClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         if (viewMode === ViewModes.formulas) {
-            dispatch(changeViewMode(ViewModes.values));
+            dispatch(applicationSlice.actions.viewModeChanged({ newMode: ViewModes.values }));
+        } else if (viewMode === ViewModes.values) {
+            dispatch(applicationSlice.actions.viewModeChanged({ newMode: ViewModes.move }));
         } else {
-            dispatch(changeViewMode(ViewModes.formulas));
+            dispatch(applicationSlice.actions.viewModeChanged({ newMode: ViewModes.formulas }));
         }
     };
 
     const elements = allIDs.map((id, index) => {
         switch (elementsByID[id].elementType) {
             case ElementTypes.inputCell:
-                return <InputCell key={index} accessID={id} />;
+                return <InputCell key={index} id={id} />;
             case ElementTypes.displayCell:
-                return <DisplayCell key={index} accessID={id} />;
+                return <DisplayCell key={index} id={id} />;
             default:
                 return "Error"; // shouldn't ever get here
         }
@@ -54,10 +65,10 @@ function Form(props: FormProps) {
 
     return (
         <div>
-            <h1>{viewMode === ViewModes.formulas ? "Formula" : "Value"} Mode</h1>
+            <h1>{viewMode}</h1>
             <button onClick={handleAddInputCellClick}>Add Input Cell</button>
             <button onClick={handleAddDisplayCellClick}>Add Display Cell</button>
-            <button onClick={handleToggleFormulaViewClick}>Toggle Formula View</button>
+            <button onClick={handleToggleViewModeClick}>Change View Mode</button>
             {elements}
         </div>
     );
